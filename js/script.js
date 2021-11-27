811b63278a74a9f5fd05d0735722b46c59954bc9
Cristian Ortiz <ortizarg987@gmail.com>
2021-11-27 10:13:28 -0300

    review section

--- js/script.js
+++ js/script.js
@@ -13,11 +13,33 @@ window.onscroll = () =>{
 
 
 var swiper = new Swiper(".home-slider", {
-    spaceBetween: 20,
-    effect: "fade",
-    loop:true,
+  spaceBetween: 20,
+  effect: "fade",
+  grabCursor:true,
+  loop:true,
+  centeredSlides:true,
     pagination: {
       el: ".swiper-pagination",
       clickable: true,
     },
-  });
+});
+
+var swiper = new Swiper(".review-slider", {
+  spaceBetween: 20,
+  effect: "fade",
+  grabCursor:true,
+  loop:true,
+  centeredSlides:true,
+  autoplay:{
+    delay:7500,
+    disableOnInteraction:false,
+  },
+  breakpoints:{
+    0:{
+      slidesPerView:1,
+    },
+    600:{
+      slidesPerView:2,
+    }
+  }
+});
