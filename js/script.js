d62d3e2ff047a90ffff366b29d5e6335475a0ee8
Cristian Ortiz <ortizarg987@gmail.com>
2021-11-27 13:02:30 -0300

    blogs section

--- js/script.js
+++ js/script.js
@@ -4,13 +4,12 @@ let navbar = document.querySelector('.header .navbar');
 menu.onclick = () =>{
     menu.classList.toggle('fa-times')
     navbar.classList.toggle('active')
-}
+};
 
 window.onscroll = () =>{
     menu.classList.remove('fa-times')
     navbar.classList.remove('active')
-}
-
+};
 
 var swiper = new Swiper(".home-slider", {
   spaceBetween: 20,
@@ -41,5 +40,30 @@ var swiper = new Swiper(".review-slider", {
     600:{
       slidesPerView:2,
     }
-  }
+  },
+});
+
+var swiper = new Swiper(".blogs-slider", {
+  spaceBetween: 20,
+  grabCursor:true,
+  loop:true,
+  autoplay:{
+    delay:7500,
+    disableOnInteraction:false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints:{
+    0:{
+      slidesPerView:1,
+    },
+    768:{
+      slidesPerView:2,
+    },
+    991:{
+      slidesPerView:3,
+    },
+  },
 });
